70f95343f15c25f47f350c4946c61e62232d2fc9
HawesJM <joshua.m.hawes@gmail.com>
2026-01-15 17:52:22 +0000

    add post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_posts, name='all_posts'),
+    path('<post_id>', views.post_detail, name='post_detail'),
 ] 
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 # Create your views here.
 
 def all_posts(request):
-    posts = Post.objects.all().order_by('-created_on')
+    posts = Post.objects.all().order_by('created_on')
+    latest_post = posts.last()
 
     context = {
         'posts': posts, 
+        'latest_post': latest_post,
     }
 
     return render(request, 'blog/blog.html', context)
+
+def post_detail(request, post_id):
+
+    post = get_object_or_404(Post, pk=post_id)
+
+    context = {
+        'post': post,
+        
+    }
+
+    return render(request, 'blog/post_detail.html', context)
