a3475f0bd5dd909afb9ad50d041030eaf45a54ea
HawesJM <joshua.m.hawes@gmail.com>
2026-01-17 13:16:03 +0000

    add copywright 2026

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 
 def all_posts(request):
     query = None
     posts = Post.objects.all().order_by('created_on')
     latest_post = posts.last()
+    categories = Category.objects.all()
 
     if request.GET:
         if 'q' in request.GET:
@@ -23,17 +24,19 @@ def all_posts(request):
         'posts': posts, 
         'latest_post': latest_post,
         'search_term': query,
+        'categories': categories,
     }
 
     return render(request, 'blog/blog.html', context)
+
 
 def post_detail(request, post_id):
 
     post = get_object_or_404(Post, pk=post_id)
 
     context = {
-        'post': post,
-        
+        'post': post,      
     }
 
     return render(request, 'blog/post_detail.html', context)
+
