b751c2c572a6e064c7e76b66a897ce0e4afe2cb0
HawesJM <joshua.m.hawes@gmail.com>
2026-01-29 18:30:19 +0000

    import articles into home view

--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
+from articles.models import Article
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    articles = Article.objects.all().order_by('-published_date')
+    latest_article = articles.first()
+    penultimate_article = articles[1] if articles.count() > 1 else None
+
+    context = {
+        'articles': articles,
+        'latest_article': latest_article,
+        'penultimate_article': penultimate_article,
+    }
+    return render(request, 'home/index.html', context)
